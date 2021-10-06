b84fbf2551610cac71bf9e4fa73edaa137750a2c
Alexei Savostyanov <lexsava@gmail.com>
2021-10-06 09:12:49 +0300

    feat: add nterface segregation principle

--- SOLID-YouTube/4_I.js
+++ SOLID-YouTube/4_I.js
@@ -1,1 +1,44 @@
 //Interface segregation
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const swimmer = {
+  swim() {
+    console.log(`${this.name} умеет плавать`);
+  },
+};
+
+const flier = {
+  fly() {
+    console.log(`${this.name} умеет летать`);
+  },
+};
+
+const walker = {
+  walk() {
+    console.log(`${this.name} умеет ходить`);
+  },
+};
+
+class Dog extends Animal {}
+
+class Eagle extends Animal {}
+
+class Whale extends Animal {}
+
+Object.assign(Dog.prototype, swimmer, walker);
+Object.assign(Eagle.prototype, flier, walker);
+Object.assign(Whale.prototype, swimmer);
+
+const dog = new Dog("Рекс");
+dog.swim();
+dog.walk();
+const eagle = new Eagle("Орел");
+eagle.fly();
+eagle.walk();
+const whale = new Whale("Большой белый друг");
+whale.swim();
